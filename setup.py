63a6553bfa930ac0c539956fed11e30279094297
Andy McKay <amckay@mozilla.com>
2012-09-21 15:35:34 -0700

    fix up manifest

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='django-mysql-aesfield',
-    version='0.2',
+    version='0.2.1',
     description='Django Model Field that supports AES encryption in MySQL',
     long_description=open('README.rst').read(),
     author='Andy McKay',
@@ -13,6 +13,9 @@ setup(
     url='https://github.com/andymckay/django-mysql-aesfield',
     include_package_data=True,
     zip_safe=False,
+    packages=['aesfield',
+              'aesfield/management',
+              'aesfield/management/commands'],
     classifiers=[
         'Intended Audience :: Developers',
         'Natural Language :: English',
